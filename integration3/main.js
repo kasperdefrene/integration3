47d41dad97d3d8c334ab7442cc22e793d7e59a21
kasperdefrene <kasper.de.frene@student.howest.be>
2023-01-20 14:07:58 +0100

    fri 20 jan push

--- integration3/main.js
+++ integration3/main.js
@@ -1,2 +1,29 @@
 import './css/reset.css'
 import './css/style.css'
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.config({ ignoreMobileResize: true});
+
+
+// create
+let mm = gsap.matchMedia();
+
+// add a media query. When it matches, the associated function will run
+mm.add("(min-width: 800px)", () => {
+
+  // this setup code only runs when viewport is at least 800px wide
+  //gsap.to(...);
+  //gsap.from(...);
+  //ScrollTrigger.create({...}); 
+
+  return () => { // optional
+    // custom cleanup code here (runs when it STOPS matching)
+  };
+});
+
+// later, if we need to revert all the animations/ScrollTriggers...
+mm.revert();
+
+init();
