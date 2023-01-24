7b57789aa657d94e3dac2e5a06599cf3e6a7dff8
kasperdefrene <kasper.de.frene@student.howest.be>
2023-01-24 22:55:59 +0100

    tuesday evening

--- integration3/main.js
+++ integration3/main.js
@@ -6,9 +6,89 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 
+window.addEventListener('DOMContentLoaded', () => {
+    gsap.from(".fold__background", {
+        opacity: 0,
+        duration: 1,
+    });
+
+    gsap.from(".fold__title",{
+        opacity: 0,
+        duration: 2,
+    });
+});
+
+// const loadTL = gsap.timeline({
+//     duration: 5,
+//     scrollTrigger:{
+//         scroller: ".container",
+//         trigger: ".j2018",
+//         start: "top top",
+//         toggleActions: "restart none reverse reset",
+//         scrub: 2,
+//     }
+// });
+
+// loadTL.from(".fold__background", {
+//     opacity: 0,
+//     ease: Power2.easeOut,
+//     duration: 1,
+// },1
+// );
+// loadTL.from(".fold__title", {
+//     opacity: 0,
+//     ease: Power2.easeOut,
+//     duration: 1,
+// },2
+// );
+
+const imageStack = () => {
+    const stackTL = gsap.timeline({
+        duration: 5,
+        scrollTrigger:{
+            trigger: ".image__stack",
+            start: "-=200 top",
+            toggleActions: "restart none reverse reset"
+        }
+    });
+
+    stackTL.from(".image__stack__title", {
+        x: -200,
+    }, 1
+    );
+    stackTL.from(".image__stack__flag", {
+        x: 200,
+    }, 1
+    );
+    stackTL.from(".image__stack__1", {
+        x: 1000,
+    }, 2
+    );
+    stackTL.from(".image__stack__2", {
+        x: -1000,
+    }, 3
+    );
+    stackTL.from(".image__stack__3", {
+        x: 1000,
+    }, 4
+    );
+    
+}
+
+
+
+
+const playing = false;
+
 const topFunction = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
+}
+
+const spinningVinyl = () => {
+    if (playing == true) {
+        document.querySelector(".turntable__vinyl").classList.add("spin");
+    }
 }
 
 // let mm = gsap.matchMedia();
@@ -33,6 +113,8 @@ const topFunction = () => {
 const init = () => {
     let topButton = document.querySelector(".top_button");
     topButton.addEventListener("click", topFunction);
+    spinningVinyl();
+    imageStack();
 }
 
 init();
